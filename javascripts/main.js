1f427185b584cf2565303c5096d1fdd8ce7b593f
utleroy <bryanhankins@me.com>
2016-10-19 20:02:57 -0500

    added promise

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,32 @@
 "use strict";
 
 $(document).ready(function() {
-	console.log("jquery working")
-})
+	console.log("jquery working");
+	let robotContent = $("#robots");
+	let robots = [];
+
+	function getRobots() {
+		return new Promise((resolve, reject) => {
+			$.ajax({
+				url: "./robot.json"
+			}).done(function(data) {
+				resolve(data);
+			}).fail(function(xhr, status, error) {
+				reject(error);
+			})
+		})
+	}
+	getRobots().then(function(dataPass) {
+		console.log("generate array", robots);
+	});
+		let robotData = "";
+		var currentRobot;
+
+		for (let i = 0; i < robots.length; i++) {
+			currentRobot = robots[i];
+
+			robotData += `<div>Robots</div>`;
+		}
+		robotContent.html(robotData)
+		 console.log("robot data", robotData);
+});
