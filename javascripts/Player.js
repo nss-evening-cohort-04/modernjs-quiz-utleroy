6d3add0191a1e6aed569cef8b043890194fcab4e
utleroy <bryanhankins@me.com>
2016-11-06 19:40:40 -0600

    dom inneracting

---
+++ javascripts/Player.js
@@ -0,0 +1,66 @@
+"use strict";
+
+var Battledome = Battledome || {};
+Battledome.BattleBot = {};
+let fightview = {};
+
+Battledome.BattleBot.Player1 = function(name1) {
+  this.type = null;
+  this.class = null;
+  this.model_specialty = null;
+
+  this.player1Name = name1 || "leroy";
+  this.health = Math.floor(Math.random() * 40 + 50);
+  this.strength = 90;
+  this.totalDamage = 1;
+
+    var outputPlayer1 = [this.player1Name + " " + this.health];
+    // console.log(outputPlayer1);
+    return outputPlayer1;
+};
+
+Battledome.BattleBot.Player2 = function(name2) {
+  this.type = null;
+  this.class = null;
+  this.model_specialty = null;
+
+  this.player2Name = name2 || "Enemy's Tank";
+  this.health = Math.floor(Math.random() * 60 + 120);
+  this.strength = 180;
+  this.totalDamage = 1;
+
+    var outputPlayer2 = [this.player2Name + " " + this.health];
+    // console.log(outputPlayer2);
+    return outputPlayer2;
+};
+Battledome.BattleBot.Player1.prototype = new Battledome.BattleBot.Player1();
+Battledome.BattleBot.Player2.prototype = new Battledome.BattleBot.Player2();
+
+
+
+
+
+
+
+
+
+
+// drone {
+//     type: drone1;
+//   model_specialty: kicking ass
+// }
+
+// drone1 {
+//   type: drone1;
+//   class: drone;
+//   model_specialty: kicking ass
+//   this.health = Math.floor(Math.random() * 60 + 120);
+//   this.strength = 180;
+//   this.totalDamage = 1;
+// }
+// drone1 {
+//   class: drone;
+//   this.health = Math.floor(Math.random() * 60 + 120);
+//   this.strength = 180;
+//   this.totalDamage = 1;
+// }
--- javascripts/main.js
+++ javascripts/main.js
@@ -2,9 +2,10 @@
 
 $(document).ready(function() {
 
+	let Bots = {};
+
 	let player1Name = "";
 	let player2Name = "";
-	let results = $("#fightResults");
 	let fightBtn = $("#fight-btn");
 	let player1 = {};
 	let player2 = {};
@@ -17,8 +18,23 @@ $(document).ready(function() {
     player1.type = player1Select;
 
     let player2Select = $("#player2Select").find(":selected").val();
-    player2.type = player2Select;
+    player2.robot = player2Select;
 
-		console.log("player values", player1, player2);
+		console.log("player name value", player1.name, player2.name);
+		// console.log("player type", player1, player2);
+		return player.name;
 	});
+
+	let battleview = $("#fightResults");
+	battleview.html(`
+		<div>
+		Intitial Player1 Health: <br>
+		<h1>${Battledome.BattleBot.Player1()}</h1> <br>
+		
+		Intitial Player2 Health: <br>
+		<h1>${Battledome.BattleBot.Player2()}</h1> <br>
+		`
+		);
+
+
 });	
